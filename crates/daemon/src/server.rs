73dd95bff2ddb36ae34b2002d6c0eb670e4e2f95
Jens Meindertsma <mail@jensmeindertsma.com>
2024-07-21 15:14:43 +0000

    do http 2

--- crates/daemon/src/server.rs
+++ crates/daemon/src/server.rs
@@ -1,28 +1,24 @@
 mod proxy;
 
 use super::configuration::Configuration;
-use hyper::{
-    rt::{Read, Write},
-    server::conn::http1::Builder,
+use hyper_util::{
+    rt::{TokioExecutor, TokioIo},
+    server::conn::auto::Builder as ConnectionBuilder,
+    service::TowerToHyperService,
 };
-use hyper_util::{rt::TokioIo, service::TowerToHyperService};
 use proxy::Proxy;
 use sailor_config::Configurable;
 use std::{net::SocketAddr, sync::Arc};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tracing::{error, info};
 
 pub struct Server {
     config: Arc<Configuration>,
-    connection_builder: Arc<Builder>,
 }
 
 impl Server {
     pub fn with_config(config: Arc<Configuration>) -> Self {
-        Self {
-            config,
-            connection_builder: Arc::new(Builder::new()),
-        }
+        Self { config }
     }
 
     pub async fn start(&self) {
@@ -33,30 +29,25 @@ impl Server {
             .unwrap_or_else(|_| panic!("binding to localhost:{port} failed!"));
 
         while let Ok((stream, address)) = listener.accept().await {
-            self.handle_connection(TokioIo::new(stream), address).await
+            self.handle_connection(stream, address).await
         }
     }
 
-    async fn handle_connection<S>(&self, stream: S, client_address: SocketAddr)
-    where
-        S: Read + Write + Send + Unpin + 'static,
-    {
+    async fn handle_connection(&self, stream: TcpStream, client_address: SocketAddr) {
         info!("handling connection from {}", client_address);
 
-        let connection_builder = self.connection_builder.clone();
         let configuration = self.config.clone();
         let proxy = TowerToHyperService::new(Proxy::new(configuration));
+        let connection = ConnectionBuilder::new(TokioExecutor::new())
+            .serve_connection(TokioIo::new(stream), proxy)
+            .await;
 
-        tokio::spawn(async move {
-            let result = connection_builder.serve_connection(stream, proxy).await;
-
-            match result {
-                Ok(_) => info!("Finished serving connection to {}", client_address),
-                Err(e) => error!(
-                    "error while serving connection to {}: {:?}",
-                    client_address, e
-                ),
-            }
-        });
+        match connection {
+            Ok(_) => info!("Finished serving connection to {}", client_address),
+            Err(e) => error!(
+                "error while serving connection to {}: {:?}",
+                client_address, e
+            ),
+        }
     }
 }
