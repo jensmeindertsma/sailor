bcfb06d5e1e8f2b762d33960b9140d2d61e6c9e8
Jens Meindertsma <mail@jensmeindertsma.com>
2024-07-21 19:04:56 +0000

    Delete UFW setup script

--- install/ufw-setup.sh
+++
@@ -1,8 +0,0 @@
-echo "Setting up UFW firewall application profile"
-
-cp ./ufw.ini /etc/ufw/applications.d/sailor
-chown root /etc/ufw/applications.d/sailor
-chmod 0600 /etc/ufw/applications.d/sailor
-ufw allow Sailor
-
-exit
